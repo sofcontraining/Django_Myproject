f2475f401521fa121f41228a9bba754c2d619478
vikaspython15 <vikassharma.dei15@gmail.com>
2023-02-15 11:47:01 +0530

    member model created

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Member (models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    contact = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name + ' ' + self.email + ' ' + self.contact
