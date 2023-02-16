175ab5047bb02500a3733cd4a034e6dc79b6748e
vikaspython15 <vikassharma.dei15@gmail.com>
2023-02-16 11:59:11 +0530

    data sent to database

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from . models import Member
 
 # Create your views here.
 def home(request):
+    all_members = Member.objects.all
     # return HttpResponse("This is Home page")
-    return render(request, 'home.html')
-
+    return render(request, 'home.html',{'all':all_members})
 def about(request):
   #  return HttpResponse("This is About page")
      return render(request, 'about.html')
@@ -12,5 +13,12 @@ def services(request):
     #return HttpResponse("This is Serives page")
     return render(request, 'services.html')
 def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        contact = request.POST['contact']
+        city = request.POST['city']
+        details = Member(name=name, email=email, contact=contact, city=city)  
+        details.save()      
     #return HttpResponse("This is Contact page")
     return render(request, 'contact.html')
--- myproject/settings.py
+++ myproject/settings.py
@@ -75,19 +75,19 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myprojectdjango',
-        'USER': 'root',
-        'PASSWORD': 'sofcon15@',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'myprojectdjango',
+    #     'USER': 'root',
+    #     'PASSWORD': 'sofcon15@',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
